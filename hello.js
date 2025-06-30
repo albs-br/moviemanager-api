119990941963a5b914f028f744d9b13eae2a8b7d
Andre Baptista <albs_br@yahoo.com.br>
2025-06-30 19:11:37 -0300

    v.0.7.6

--- hello.js
+++ hello.js
@@ -1,3 +1,21 @@
 console.info("hello world");
 console.info(process.env.AUTH0_CLIENT_SECRET);
 console.info(process.env.PORT);
+
+
+
+
+// minimum app for testing
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route for the root URL ("/")
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
